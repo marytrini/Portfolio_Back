5a7b95674931ebf49b0f4c1082eeccdf27c4e792
Maria Salazar <mariatrinidadsalazarr@gmail.com>
2023-11-24 08:49:22 -0400

    index_and_url

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const app = require('./src/app.js');
 const {conn} = require('./src/db.js');
 
+const PORT = 3131;
+
 conn.sync({force: false}).then(()=>{
     console.log("DB connected to server");
-    app.listen(3001, ()=>{
-        console.log("%s listening at 3001");
+    app.listen(PORT, ()=>{
+        console.log(`server listening at ${PORT}`);
     })
 })
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 const {Router} = require('express');
-const contactRouter = require('./contactRouter')
+const contactRouter = require('./contactRouter');
+
 
 const router = Router();
 
 router.use("/contact", contactRouter)
+
 
 module.exports = router;
